e8c184350d46b1c2c1621abe0b05f96611883529
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-01-19 17:32:55 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config();
-
 const app = express();
+
+// middlewares
+app.use(express.json())
+
+// database connection
 mongoose.connect(process.env.DBURL)
     .then(() => console.log("connected to DataBase"))
     .catch((error) => {
         console.log(`Error occure in Database Connection : ${error}`)
     })
 
+    
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port 3000 `);
 })
