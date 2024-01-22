82101f17e44a2667bbf0be8a3775e5c36bd8fde6
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-01-22 22:56:41 +0530

    complete upload listing functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -3,37 +3,37 @@ import React from 'react'
 const CreateListing = () => {
     return (
         <main className='p-5 max-w-6xl mx-auto'>
-            <h1 className='sm:text-4xl text-2xl text-gray-700 font-bold text-center my-7 '>
+            <h1 className='sm:text-4xl text-xl text-gray-700 sm:font-bold font-semibold text-center my-7 '>
                 Create a Listing
             </h1>
 
-            <form className='flex flex-col sm:flex-row gap-8'>
+            <form className='flex flex-col sm:flex-row sm:gap-8 gap-5'>
                 {/*  first side*/}
-                <div className='flex flex-col gap-6 flex-1 '>
+                <div className='flex flex-col gap-4 sm:gap-6 flex-1 '>
 
                     <input className='border p-3 rounded-lg ' type="text" placeholder='Name' id='name' maxLength='62' minLength='10' required />
                     <input className='border p-3 rounded-lg ' type="text" placeholder='Description' id='description' required />
                     <input className='border p-3 rounded-lg ' type="text" placeholder='Address' id='address' required />
-                    <div className='flex flex-wrap gap-5'>
+                    <div className='flex flex-wrap gap-5 '>
                         <div className='flex gap-5 items-center'>
                             <input type='checkbox' id='sale' className='w-8 h-8' />
-                            <span className='text-xl font-semibold  text-slate-700'>Sale</span>
+                            <span className='sm:text-xl text-lg font-semibold  text-slate-700'>Sale</span>
                         </div>
                         <div className='flex gap-5 items-center'>
                             <input type='checkbox' id='rent' className='w-8 h-8' />
-                            <span className='text-xl font-semibold  text-slate-700'>Rent</span>
+                            <span className='sm:text-xl text-lg font-semibold  text-slate-700'>Rent</span>
                         </div>
                         <div className='flex gap-5 items-center'>
                             <input type='checkbox' id='parking' className='w-8 h-8' />
-                            <span className='text-xl font-semibold  text-slate-700'>Parking Spot</span>
+                            <span className='sm:text-xl text-lg font-semibold  text-slate-700'>Parking Spot</span>
                         </div>
                         <div className='flex gap-5 items-center'>
                             <input type='checkbox' id='furnished' className='w-8 h-8' />
-                            <span className='text-xl font-semibold  text-slate-700'>Furnished</span>
+                            <span className='sm:text-xl text-lg font-semibold  text-slate-700'>Furnished</span>
                         </div>
                         <div className='flex gap-5 items-center'>
                             <input type='checkbox' id='offer' className='w-8 h-8' />
-                            <span className='text-xl font-semibold text-slate-700 '>Offer</span>
+                            <span className='sm:text-xl text-lg font-semibold text-slate-700 '>Offer</span>
                         </div>
                     </div>
 
@@ -46,7 +46,7 @@ const CreateListing = () => {
                                 max='10'
                                 required
                                 className='p-3 border border-gray-300 rounded-lg ' />
-                            <p className='text-xl font-semibold  text-slate-700'>Beds</p>
+                            <p className='sm:text-xl text-lg font-semibold  text-slate-700'>Beds</p>
                         </div>
                         <div className='flex gap-5 items-center '>
                             <input id="bathrooms"
@@ -55,7 +55,7 @@ const CreateListing = () => {
                                 max='10'
                                 required
                                 className='p-3 border border-gray-300 rounded-lg ' />
-                            <p className='text-xl font-semibold  text-slate-700'>Baths</p>
+                            <p className='sm:text-xl text-lg font-semibold  text-slate-700'>Baths</p>
                         </div>
                         <div className='flex gap-5 items-center '>
                             <input id="regularPrice"
@@ -66,9 +66,9 @@ const CreateListing = () => {
                                 className='p-3 border border-gray-300 rounded-lg ' />
 
                             <div className='text-xl font-semibold text-slate-700 flex flex-col gap-1 items-center' >
-                                <p >  Regular Price </p>
+                                <p  className='sm:text-xl text-lg font-semibold  text-slate-700'>  Regular Price </p>
 
-                                <p className='text-lg'>(₹ / Month)</p>
+                                <p className='sm:text-lg text-sm'>(₹ / Month)</p>
                             </div>
 
                         </div>
@@ -80,23 +80,23 @@ const CreateListing = () => {
                                 required
                                 className='p-3 border border-gray-300 rounded-lg ' />
                             <div className='text-xl font-semibold text-slate-700 flex flex-col items-center gap-1' >
-                                <p >  Discount Price </p>
-                                <p className='text-lg'>(₹ / Month)</p>
+                                <p  className='sm:text-xl text-lg font-semibold  text-slate-700'>  Discount Price </p>
+                                <p className='sm:text-lg text-sm'>(₹ / Month)</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* second side */}
                 <div className='flex flex-col items-center gap-6 flex-1'>
-                    <p className='font-bold text-2xl '>
+                    <p className='sm:font-bold font-semibold sm:text-2xl text-lg'>
                         Images:
-                        <span className='font-normal text-gray-600 ml-3 '>The first image will be the cover (max 6)</span>
+                        <span className='font-normal text-gray-600 text-sm sm:text-xl  ml-3 '>The first image will be the cover (max 6)</span>
                     </p>
-                    <div className='flex gap-5 border p-2 border-gray-200 '>
-                        <input type='file' id='images' accept='image/*' multiple className='p-4 pl-0  border-gray-300 rounded text-2xl  w-full' />
-                        <button className='p-4 text-green-700 font-semibold text-xl  border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80 '>Upload</button>
+                    <div className='flex gap-5 border p-1 border-gray-200 '>
+                        <input type='file' id='images' accept='image/*' multiple className='sm:p-4 p-2  border-gray-300 rounded sm:text-xl md:text-2xl text-lg  w-full' />
+                        <button className='sm:p-4 p-2 text-green-700 font-semibold sm:text-xl md:text-2xl text-lg  border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80 '>Upload</button>
                     </div>
-                    <button className='p-4 w-full text-2xl  bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '>
+                    <button className='sm:p-4 p-2 w-full sm:text-2xl text-lg  bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '>
                         Create List
                     </button>
                 </div>
