6ff069d86efb43dc30a926b233fdb0ef33450358
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-01-26 23:28:59 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,56 +1,52 @@
 const express = require('express')
 const mongoose = require('mongoose')
-require('dotenv').config();
-const app = express();
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
-
-
-
+const path = require('path');
 const userroute = require('./routes/userRoute.js')
 const authRouter = require('./routes/authRoute.js');
 const listingRouter = require('./routes/listingRouter.js');
+require('dotenv').config();
 
-// middlewares
+// const cors = require('cors');
+
+mongoose.connect(process.env.DBURL)
+  .then(() => console.log("connected to DataBase"))
+  .catch((error) => {
+    console.log(`Error occure in Database Connection : ${error}`)
+  })
+
+__dirname = path.resolve();
+
+const app = express();
 app.use(express.json())
 app.use(cookieParser());
-app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
-
-// database connection
-mongoose.connect(process.env.DBURL)
-    .then(() => console.log("connected to DataBase"))
-    .catch((error) => {
-        console.log(`Error occure in Database Connection : ${error}`)
-    })
-
-
-
 app.listen(process.env.PORT, () => {
-    console.log(`server is running on port ${process.env.PORT}`);
+  console.log(`server is running on port ${process.env.PORT}`);
 })
-
 app.use('/api/user', userroute);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hey there</h1>");
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
-  
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error"
-
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    })
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
+
+
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
