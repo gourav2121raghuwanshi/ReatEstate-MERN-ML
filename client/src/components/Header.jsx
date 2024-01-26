77ac737baeb3eb66350cb15a39ec521211d35d30
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-01-26 11:55:04 +0530

    update Header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,43 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { FaSearch } from "react-icons/fa";
-import {  useSelector } from 'react-redux';
-import { TypeAnimation } from 'react-type-animation';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 const Header = () => {
   const { currentUser } = useSelector(state => state.user);
-
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermfromUrl = urlParams.get('searchTerm');
+    if(searchTermfromUrl){
+      setSearchTerm(searchTermfromUrl);
+    }
+  }, [location.search])
   return (
     <header className='bg-slate-200 shadow-md '>
       <div className='flex justify-between items-center max-w-7xl  mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold hover:cursor-pointer text-sm sm:text-xl flex flex-wrap '>
-
-          {/* <TypeAnimation
-                            sequence={["FindYourHome",2000,""]}
-                            repeat={Infinity}
-                            cursor={true}
-                            omitDeletionAnimation={true}  //the written down text will not be deleted
-                            style={
-                                {
-                                    whiteSpace:"pre-line", //on this parameter we will shift to new line
-                                    display:"block",   //to shift to new line
-                                }
-                            }
-                    /> */}
-
-
-
             <span className='text-slate-500 font-bold md:text-2xl lg:text-4xl'>FindYour</span>
             <span className='text-slate-700 font-bold md:text-2xl lg:text-4xl'>Home</span>
           </h1>
         </Link>
-        <form className='bg-slate-100  flex items-center justify-between p-3 rounded-lg '>
+        <form onSubmit={handleSubmit} className='bg-slate-100  flex items-center justify-between p-3 rounded-lg '>
           <input
             type="text"
             placeholder='Search...'
             className='bg-transparent font-semibold md:text-lg lg:text-2xl hover:cursor-pointer focus:outline-none w-24 sm:w-64' // to mobile->24 and after mobile->64
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className='text-slate-600 hover:cursor-pointer' />
+          <button >
+            <FaSearch className='text-slate-600 hover:cursor-pointer' />
+          </button>
         </form>
         <ul className='flex gap-4 text-sm sm:text-base md:text-lg lg:text-2xl'>
           <Link to='/'>
