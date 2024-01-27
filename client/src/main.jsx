81c9ff4d3330dc09ce595e46ca4ff290230cab95
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-01-27 11:40:38 +0530

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,26 +5,12 @@ import './index.css'
 import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-        
-      />
     </PersistGate>
   </Provider>
 )
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom';
-// import ClipLoader from "react-spinners/ClipLoader";
 import OAuth from '../components/OAuth';
 const SignUp = () => {
 
